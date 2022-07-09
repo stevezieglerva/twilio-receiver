7f93df32a1c67b9f33996770b68f8ec353ae54e9
Steve Ziegler <stephen.v.ziegler@gmail.com>
2022-07-08 22:45:40 -0400

    update hello world

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -33,6 +33,8 @@ def lambda_handler(event, context):
 
     #     raise e
 
+    print(json.dumps(event, indent=3, default=str))
+
     return {
         "statusCode": 200,
         "body": json.dumps({
