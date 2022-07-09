7b2fbdd01dea3e71f263c77b92343bf23cd4f6c4
Steve Ziegler <stephen.v.ziegler@gmail.com>
2022-07-09 10:24:51 -0400

    config test working

--- sam-app/tests/test_unit_ReminderConfig.py
+++ sam-app/tests/test_unit_ReminderConfig.py
@@ -1,13 +1,13 @@
 import unittest
 from unittest.mock import MagicMock, Mock, PropertyMock, patch
 
-from common_layer.python.domain.Reminders import RemindersConfig
+import RemindersDTO
 
 
 class ReminderConfigUnitTests(unittest.TestCase):
     def test_should_add_reminder(self):
         # Arrange
-        subject = RemindersConfig()
+        subject = RemindersDTO.RemindersConfig()
 
         # Act
         results = subject.add_reminder("Take medicine", ["09:00", "10:00"])
