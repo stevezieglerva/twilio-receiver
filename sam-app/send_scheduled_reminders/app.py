c286829a05f0fe4568c10f17ef44e7f796a3205c
Steve Ziegler <stephen.v.ziegler@gmail.com>
2022-07-11 11:58:03 -0400

    revert to 3.7

--- sam-app/send_scheduled_reminders/app.py
+++ sam-app/send_scheduled_reminders/app.py
@@ -1,44 +1,22 @@
 import json
 
-# import requests
+import Clock
+import RemindersDTO
+import ReminderSender
+import StorageRepo
+
+import SendAdapter
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    print(f"event: {event}")
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    clock = Clock.RealClock()
+    repo = StorageRepo.S3Repo("x", "ci_test")
+    repo.save_reminder(RemindersDTO.Reminder("Take medicine", ["09:00", "10:00"]))
+    reminder_sender = ReminderSender.ReminderSender(config, clock, repo)
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    print(json.dumps(event, indent=3, default=str))
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    subject = SendAdapter.SendAdapter(reminder_sender)
+    results = subject.send_reminders()
+    print(f"reminders sent: {json.dumps(results, indent=3, default=str)}")
+    return results
