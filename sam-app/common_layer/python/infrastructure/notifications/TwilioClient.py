427f685eb349641021152280e848a3f279a5f25f
Steve Ziegler <stephen.v.ziegler@gmail.com>
2022-07-12 12:59:02 -0400

    Twilio int test working

--- sam-app/common_layer/python/infrastructure/notifications/TwilioClient.py
+++ sam-app/common_layer/python/infrastructure/notifications/TwilioClient.py
@@ -3,12 +3,15 @@ from dataclasses import asdict, dataclass
 from datetime import datetime
 from typing import List
 
+from twilio.rest import Client
+
 
 @dataclass(frozen=True)
 class SMSText:
     phone_numbers: str
     text: str
     date: str
+    confirmation: str = ""
 
 
 class IProcessingTexts(ABC):
@@ -27,7 +30,28 @@ class FakeTwilio(IProcessingTexts):
         self._twilio_auth_token = twilio_auth_token
 
     def send_text(self, phone_number: str, text: str) -> str:
+
         return SMSText(phone_number, text, datetime.now().isoformat())
+
+    def receive_text(self, raw_webhook_body: str) -> str:
+        return ""
+
+
+class Twilio(IProcessingTexts):
+    def __init__(self, twilio_acount_sid: str, twilio_auth_token: str):
+        self._twilio_acount_sid = twilio_acount_sid
+        self._twilio_auth_token = twilio_auth_token
+        self._client = Client(twilio_acount_sid, twilio_auth_token)
+
+    def send_text(self, phone_number: str, text: str) -> str:
+
+        message = self._client.messages.create(
+            body="This is the ship that made the Kessel Run in fourteen parsecs?",
+            from_="+19894655460",
+            to=phone_number,
+        )
+        print(message)
+        return SMSText(phone_number, text, datetime.now().isoformat(), message.sid)
 
     def receive_text(self, raw_webhook_body: str) -> str:
         return ""
--- sam-app/tests/integration/test_int_twilio.py
+++ sam-app/tests/integration/test_int_twilio.py
@@ -6,19 +6,21 @@ from unittest.mock import MagicMock, Mock, PropertyMock, patch
 from infrastructure.notifications.TwilioClient import *
 
 
-class FakeTwilioUnitTests(unittest.TestCase):
+class TwilioUnitTests(unittest.TestCase):
     def test_should_send_text(self):
         # Arrange
-        subject = FakeTwilio("abc", "123")
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+        subject = Twilio(account_sid, auth_token)
 
         # Act
         results = subject.send_text(
-            "+11234567890", f"Here's a test message at {datetime.now()}"
+            "+19193229617", f"Here's a test message at {datetime.now()}"
         )
         print(f"test results: {results}")
 
         # Assert
-        self.assertTrue(results.date != "")
+        self.assertTrue(results.confirmation != "")
 
 
 if __name__ == "__main__":
