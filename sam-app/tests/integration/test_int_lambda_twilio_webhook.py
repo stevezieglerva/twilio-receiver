27b8188ffa62b42f35781bf92762c8b23998bd63
Steve Ziegler <stephen.v.ziegler@gmail.com>
2022-07-15 12:48:46 -0400

    removed test

--- sam-app/tests/integration/test_int_lambda_twilio_webhook.py
+++ sam-app/tests/integration/test_int_lambda_twilio_webhook.py
@@ -3,18 +3,17 @@ from unittest.mock import MagicMock, Mock, PropertyMock, patch
 
 from twilio_webhook.app import *
 
+# class TwilioWebhookIntTests(unittest.TestCase):
+#     def test_should_process_done(self):
+#         # Arrange
+#         subject = Class()
 
-class TwilioWebhookIntTests(unittest.TestCase):
-    def test_should_process_done(self):
-        # Arrange
-        subject = Class()
+#         # Act
+#         results = subject.method()
+#         print(f"test results: {results}")
 
-        # Act
-        results = subject.method()
-        print(f"test results: {results}")
-
-        # Assert
-        self.assertEqual(results, "")
+#         # Assert
+#         self.assertEqual(results, "")
 
 
 if __name__ == "__main__":
